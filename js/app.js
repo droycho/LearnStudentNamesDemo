c9d0363985470017f1c6766c8e6ce0326c4163d5
DJ Roychoudhury <debjitroychoudhury@u.boisestate.edu>
2020-04-05 21:01:14 -0600

    Add files via upload
    added initial files from class repo

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// all our functionality resides here
+$(document).ready(function() {
+    document.getElementById("newUser").onclick = function () {
+        location.href = "#";
+    };
+    document.getElementById("logIn").onclick = function () {
+        location.href = "signIn.html";
+    };
+    
+});
---
+++ js/login.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    document.getElementById("cancelButton").onclick = function () {
+        location.href = "index.html";
+    };
+});
